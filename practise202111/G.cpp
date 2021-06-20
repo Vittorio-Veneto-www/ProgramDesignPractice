2068231d25860d03a1d1dd71dfbfbf6db888e9f3
Vittorio-Veneto-www <wym19pku@pku.edu.cn>
2021-06-20 19:10:58 +0800

    Update G.cpp

--- practise202111/G.cpp
+++ practise202111/G.cpp
@@ -15,22 +15,28 @@ int main(void)
         for (int i = 0; i < n; i++)
             scanf("%d", &a[i]);
         memset(f, 0x3f, sizeof(f));
+        for (int i = 1; i < n; i++)
+            f[i][i - 1] = 0;
         for (int i = 0; i < n; i++)
             f[i][i] = 1;
+        /*for (int i = 0; i < n - 1; i++)
+            if (a[i] == a[i + 1])
+                f[i][i + 1] = 1;
+            else
+                f[i][i + 1] = 2;*/
         for (int i = 1; i < n; i++)
             for (int j = 0; j < n - i; j++)
-            {
-                if (a[j] == a[j + i])
-                    if (i == 1)
-                        f[j][j + i] = 1;
-                    else
-                        f[j][j + i] = f[j + 1][j + i - 1] + 1;
                 for (int k = 0; k < i; k++)
-                    if (a[j + k] == a[j + i] && i > 1)
+                    if (a[j + k] == a[j + i])
                         f[j][j + i] = std::min(f[j][j + i], f[j][j + k] + f[j + k + 1][j + i - 1]);
                     else
                         f[j][j + i] = std::min(f[j][j + i], f[j][j + k] + f[j + k + 1][j + i]);
-            }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+                printf("%d ", f[i][j]);
+            printf("\n");
+        }
         printf("Case #%d: %d\n", cnt, f[0][n - 1]);
     }
 }
